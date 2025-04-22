cde7ce802d64e48e2f84e8725b525a4035714d55
Spyro1322 <44666695+Spyro1322@users.noreply.github.com>
2025-04-22 12:45:48 +0300

    Update rep_revenue_per_period.sql

--- reporting/rep_revenue_per_period.sql
+++ reporting/rep_revenue_per_period.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS diesel-media-457308-t3.reporting_db.rep_revenue_per_period AS
+CREATE TABLE IF NOT EXISTS `diesel-media-457308-t3.reporting_db.rep_revenue_per_period` AS
   WITH
     reporting_periods AS (
       SELECT
@@ -6,27 +6,42 @@ CREATE TABLE IF NOT EXISTS diesel-media-457308-t3.reporting_db.rep_revenue_per_p
         reporting_date
       FROM
         diesel-media-457308-t3.reporting_db.reporting_periods
+      WHERE
+        reporting_date >= '2015-01-01'
+        AND reporting_period IN ('Day', 'Month', 'Year')
+    ),
+    revenue_data AS (
+      SELECT
+        payment.payment_amount,
+        CASE
+          WHEN rp.reporting_period = 'Day' THEN DATE(payment.payment_date)
+          WHEN rp.reporting_period = 'Month' THEN DATE_TRUNC(payment.payment_date, MONTH)
+          WHEN rp.reporting_period = 'Year' THEN DATE_TRUNC(payment.payment_date, YEAR)
+        END AS truncated_payment_date,
+        rp.reporting_period,
+        rp.reporting_date,
+        film.film_title
+      FROM
+        reporting_periods rp
+      LEFT JOIN diesel-media-457308-t3.staging_db.stg_payment AS payment
+        ON (
+          CASE
+            WHEN rp.reporting_period = 'Day' THEN DATE(payment.payment_date)
+            WHEN rp.reporting_period = 'Month' THEN DATE_TRUNC(payment.payment_date, MONTH)
+            WHEN rp.reporting_period = 'Year' THEN DATE_TRUNC(payment.payment_date, YEAR)
+          END
+        ) = rp.reporting_date
+      LEFT JOIN diesel-media-457308-t3.staging_db.stg_rental AS rental
+        ON payment.rental_id = rental.rental_id
+      LEFT JOIN diesel-media-457308-t3.staging_db.stg_inventory AS inventory
+        ON rental.inventory_id = inventory.inventory_id
+      LEFT JOIN diesel-media-457308-t3.staging_db.stg_film AS film
+        ON inventory.film_id = film.film_id
     )
   SELECT
-    reporting_periods.reporting_period,
-    reporting_periods.reporting_date,
-    COALESCE(SUM(payment.payment_amount), 0) AS total_revenue
+    reporting_period,
+    reporting_date,
+    COALESCE(SUM(CASE WHEN film_title != 'GOODFELLAS SALUTE' THEN payment_amount ELSE 0 END), 0) AS total_revenue
   FROM
-    reporting_periods
-    LEFT JOIN
-    diesel-media-457308-t3.staging_db.stg_payment AS payment
-    ON DATE(payment.payment_date) = reporting_periods.reporting_date
-    LEFT JOIN
-    diesel-media-457308-t3.staging_db.stg_rental AS rental
-    ON payment.rental_id = rental.rental_id
-    LEFT JOIN
-    diesel-media-457308-t3.staging_db.stg_inventory AS inventory
-    ON rental.inventory_id = inventory.inventory_id
-    LEFT JOIN
-    diesel-media-457308-t3.staging_db.stg_film AS film
-    ON inventory.film_id = film.film_id
-  WHERE
-    film.film_title <> 'GOODFELLAS SALUTE' AND reporting_periods.reporting_date >= '2015-01-01' AND
-    reporting_periods.reporting_period IN ('Day', 'Month', 'Year')
+    revenue_data
   GROUP BY 1, 2;
-
